0cc30c97e3e7acd681d182f4daa13dc0b6e78a13
kaechesbord <damirlihtneri6@gmail.com>
2022-10-16 09:51:24 +0200

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import "../App.css";
+import axios from "axios";
+import Swal from "sweetalert2";
+
+const Form = () => {
+  const schema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().min(8).max(32).required(),
+    ime: yup.string(),
+    prezime: yup.string()
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const onSubmitHandler = (data) => {
+    console.log({ data });
+    postUser(data);
+    reset();
+  };
+  const postUser = async (data) => {
+    await axios
+      .post("https://api.enovaapp.com/login", data)
+      .catch(() => {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Invalid Email!",
+        });
+      });
+  };
+  return (
+    <div className="position">
+      <form className="form" onSubmit={handleSubmit(onSubmitHandler)}>
+        <h1>Registracija</h1>
+        <p> Ja sam </p> 
+        <input type="radio" id="fl"/>
+        <label htmlFor="fl" className="check">Fizičko Lice</label>
+        <input type="radio" id="pl"/>
+        <label htmlFor="pl">Pravno Lice</label>
+        <div className="names">
+        <input className="input" type="text" placeholder="Ime" {...register("ime")} required />
+        <input
+          className="input"
+          type="text"
+          placeholder="Prezime"
+          {...register("prezime")}
+          required
+        />{" "}
+        </div>
+        <br />
+        <div className="grad-tel">
+        <input className="input" type="text" placeholder="Grad" {...register("email")} required />
+        <p className="error">{errors.email?.message}</p>
+        <input
+          className="input"
+          type="password"
+          placeholder="Telefon"
+          {...register("password")}
+          required
+        />{" "}
+        </div>
+        <br />
+        <p className="error">{errors.password?.message}</p>
+        <div className="mail-pass">
+        <input className="input" type="text" placeholder="Email" {...register("email")} required />
+        <p className="error">{errors.email?.message}</p>
+        <input
+          className="input"
+          type="password"
+          placeholder="Password"
+          {...register("password")}
+          required
+        />{" "}
+        </div>
+        <br />
+        <p className="error">{errors.password?.message}</p>
+        <button className="button">Registruj se</button>
+        <strong>Registrovan korisnik?</strong> <br/>
+        <a href="#">Prijavi se ovdje</a>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
